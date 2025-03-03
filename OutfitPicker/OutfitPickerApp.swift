094e0a745b6df6c711b94e2f15e7c34160008d57
HenryAdams18 <147613897+HenryAdams18@users.noreply.github.com>
2025-03-03 22:19:46 +0000

    Initial Commit

---
+++ OutfitPicker/OutfitPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutfitPickerApp.swift
+//  OutfitPicker
+//
+//  Created by Henry Adams on 03/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct OutfitPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OutfitPickerTests/OutfitPickerTests.swift
@@ -0,0 +1,17 @@
+//
+//  OutfitPickerTests.swift
+//  OutfitPickerTests
+//
+//  Created by Henry Adams on 03/03/2025.
+//
+
+import Testing
+@testable import OutfitPicker
+
+struct OutfitPickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
